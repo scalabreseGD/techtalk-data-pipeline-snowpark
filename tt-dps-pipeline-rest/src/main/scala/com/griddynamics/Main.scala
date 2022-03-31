4f75976f472c4a381d85dadc84eaee800d1d0298
scalabrese <simoncalabrese94@gmail.com>
2022-03-31 16:55:21 +0200

    Pipeline v2

--- tt-dps-pipeline-rest/src/main/scala/com/griddynamics/Main.scala
+++ tt-dps-pipeline-rest/src/main/scala/com/griddynamics/Main.scala
@@ -1,84 +1,160 @@
 package com.griddynamics
 
-import com.griddynamics.common.ConfigUtils.servlets
+import com.griddynamics.common.ConfigUtils.{pipelineConfigs, servlets}
 import com.griddynamics.common.Implicits.sessionManager
 import com.griddynamics.common.SnowflakeUtils
 import com.griddynamics.common.SnowflakeUtils.StreamSourceMode
+import com.griddynamics.common.Types.{Employee, IndustryCode}
 import com.griddynamics.pipeline.utils.HttpClientUtils
-import com.snowflake.snowpark.DataFrame
 import com.snowflake.snowpark.functions.{col, concat, lit, parse_json}
 import com.snowflake.snowpark.types.{DoubleType, StringType}
+import com.snowflake.snowpark.{DataFrame, SaveMode, Session}
 
 object Main {
 
-  private val filePath = s"industries-rest/${System.currentTimeMillis()}"
-  private val stageName = "industries_rest"
-  private val industryStreamName = "INDUSTRY_REST_STREAM"
+  private val filePath = s"employees-rest/${System.currentTimeMillis()}"
+  private val employeeStageName = "employees_rest"
+  private val employeeStreamName = pipelineConfigs.getOrElse(
+    "employee-stream",
+    throw new Error("Stream Name not present in configs")
+  )
+  private val employeeTableName = pipelineConfigs.getOrElse(
+    "employee",
+    throw new Error("Table not present in configs")
+  )
+  private val industryCodeTableName = pipelineConfigs.getOrElse(
+    "industry-code",
+    throw new Error("Table not present in configs")
+  )
 
-  def main(args: Array[String]): Unit = {
-    val session = sessionManager.get
-    val industryGeneratorUrl =
+  private val industryStreamName = pipelineConfigs.getOrElse(
+    "industry-stream",
+    throw new Error("Stream Name not present in configs")
+  )
+
+  def industryRestToIndustryTableStream(): Unit = {
+    val session: Session = sessionManager.get
+    val industryGeneratorUrl: String =
       s"${servlets.generator.baseUrl}:${servlets.generator.port}${servlets.generator.basePath}${servlets.generator.endpoints
-        .getOrElse("generate-industries", throw new Error("Endpoint missing"))}"
-//
-//    val employeeGeneratorUrl =
-//      s"${servlets.generator.baseUrl}:${servlets.generator.port}${servlets.generator.basePath}${servlets.generator.endpoints
-//        .getOrElse("generate-employees", throw new Error("Endpoint missing"))}"
-//    val employees = HttpClientUtils.performGet[List[Employee]](
-//      s"$employeeGeneratorUrl/{{numRecords}}",
-//      Map("numRecords" -> 10)
-//    )
-//    session
-//      .createDataFrame(employees)
-//      .write
-//      .mode(SaveMode.Append)
-//      .saveAsTable("EMPLOYEE_CODE_PIPELINE")
-//
-    val path = HttpClientUtils.performGetAndWrite(
-      s"$industryGeneratorUrl/{{numRecords}}",
-      Map("numRecords" -> 10),
-      filePath
-    )
-
-    SnowflakeUtils.createStage(stageName, orReplace = true, ifNotExists = false)
+        .getOrElse("generate-industries", throw new Error("Endpoint missing"))}/{{numRecords}}"
+    val industries: Array[IndustryCode] =
+      HttpClientUtils.performGet[Array[IndustryCode]](
+        url = industryGeneratorUrl,
+        Map("numRecords" -> 10)
+      )
+    session
+      .createDataFrame(industries)
+      .write
+      .mode(SaveMode.Append)
+      .saveAsTable(industryCodeTableName)
 
     SnowflakeUtils.createStreamOnObjectType(
       industryStreamName,
-      stageName,
+      sourceObjectName = industryCodeTableName,
       withReplace = true,
-      sourceObjectType = StreamSourceMode.Stage
+      sourceObjectType = StreamSourceMode.Table
     )
+  }
 
-    SnowflakeUtils.stageLocalPath(
-      stageName,
-      path,
-      filePath,
-      orReplace = false,
-      ifNotExists = true
-    )
-
+  private def fromEmployeeFileToEmployeeDf(): Unit = {
+    val session = sessionManager.get
     val streamContent = session
-      .table(industryStreamName)
-      .select(concat(lit(s"@$stageName/"), col("relative_path")))
+      .table(employeeStreamName)
+      .select(concat(lit(s"@$employeeStageName/"), col("relative_path")))
       .cacheResult()
     val filesToRead: Option[DataFrame] = streamContent
       .collect()
       .map(_.getString(0))
       .map(session.read.json)
       .reduceOption((first: DataFrame, second: DataFrame) => first union second)
+    filesToRead.foreach { df =>
+      df.select(parse_json(col("*")).as("full"))
+        .flatten(col("full"))
+        .select(
+          col("VALUE")(
+            "districtCodeFirst2"
+          ) cast StringType as "districtCodeFirst2",
+          col("VALUE")("name") cast StringType as "name",
+          col("VALUE")("surname") cast StringType as "surname"
+        )
+        .write
+        .mode(SaveMode.Append)
+        .saveAsTable(employeeTableName)
+    }
+  }
+  def employeeRestToStageStream(): Unit = {
+    val employeeGeneratorUrl =
+      s"${servlets.generator.baseUrl}:${servlets.generator.port}${servlets.generator.basePath}${servlets.generator.endpoints
+        .getOrElse("generate-employees", throw new Error("Endpoint missing"))}"
+    val employeePath = HttpClientUtils.performGetAndWrite(
+      s"$employeeGeneratorUrl/{{numRecords}}",
+      Map("numRecords" -> 10),
+      filePath
+    )
+    SnowflakeUtils.createStage(
+      employeeStageName,
+      orReplace = true,
+      ifNotExists = false
+    )
+    SnowflakeUtils.createStreamOnObjectType(
+      employeeStreamName,
+      employeeStageName,
+      withReplace = true,
+      sourceObjectType = StreamSourceMode.Stage
+    )
 
-      filesToRead.foreach { df =>
-        df.select(parse_json(col("*")).as("full"))
-          .flatten(col("full"))
-          .select(
-            col("VALUE")("departmentCode") cast StringType as "departmentCode",
-            col("VALUE")("districtCode") cast StringType as "districtCode",
-            col("VALUE")(
-              "sizeInSquareMeters"
-            ) cast DoubleType as "sizeInSquareMeters"
-          )
-          .show(10)
-      }
+    SnowflakeUtils.stageLocalPath(
+      employeeStageName,
+      employeePath,
+      filePath,
+      orReplace = false,
+      ifNotExists = true
+    )
+    fromEmployeeFileToEmployeeDf()
+  }
+
+  def parseEmployeeStageStreamToEmployeeStream(): Unit = {
+    val session = sessionManager.get
+    val streamContent = session
+      .table(employeeStreamName)
+      .select(concat(lit(s"@$employeeStageName/"), col("relative_path")))
+      .cacheResult()
+    val filesToRead: Option[DataFrame] = streamContent
+      .collect()
+      .map(_.getString(0))
+      .map(session.read.json)
+      .reduceOption((first: DataFrame, second: DataFrame) => first union second)
+  }
+
+  def main(args: Array[String]): Unit = {
+    industryRestToIndustryTableStream()
+    employeeRestToStageStream()
+    parseEmployeeStageStreamToEmployeeStream()
+
+//    val session = sessionManager.get
+//
+//    val streamContent = session
+//      .table(industryStreamName)
+//      .select(concat(lit(s"@$stageName/"), col("relative_path")))
+//      .cacheResult()
+//    val filesToRead: Option[DataFrame] = streamContent
+//      .collect()
+//      .map(_.getString(0))
+//      .map(session.read.json)
+//      .reduceOption((first: DataFrame, second: DataFrame) => first union second)
+//
+//    filesToRead.foreach { df =>
+//      df.select(parse_json(col("*")).as("full"))
+//        .flatten(col("full"))
+//        .select(
+//          col("VALUE")("departmentCode") cast StringType as "departmentCode",
+//          col("VALUE")("districtCode") cast StringType as "districtCode",
+//          col("VALUE")(
+//            "sizeInSquareMeters"
+//          ) cast DoubleType as "sizeInSquareMeters"
+//        )
+//        .show(10)
+//    }
   }
 
 }
